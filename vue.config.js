61ece26301ef93eb4d3e9f3f01d73641ffe1dc5f
Dilshat <dilshat.osmanov05@gmail.com>
2023-11-15 21:02:06 +0600

    Fix bg color

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   pwa: {
-    themeColor: 'rgba(255, 255, 255, 0)',
+    themeColor: '#fff',
     workboxOptions: {
       skipWaiting: true,
     },
-    manifestOptions: {
-      background_color: 'rgba(255, 255, 255, 0)',
-    },
     appleMobileWebAppCapable: 'yes',
-    // appleMobileWebAppStatusBarStyle: 'black',
+    appleMobileWebAppStatusBarStyle: 'rgba(255, 255, 255, 0)',
   },
 };
