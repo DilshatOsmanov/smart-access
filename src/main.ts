897a1a61ae4849bd71997d1e916ba2166d9e6eab
Dilshat <dilshat.osmanov05@gmail.com>
2023-11-15 21:28:16 +0600

    add service worker

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import { sync } from 'vuex-router-sync';
 
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
 
 sync(store, router);
 
 (createApp(App).config as any).productionTip = false;
 
-import "bootstrap";
-import "@/assets/scss/bootstrap.scss";
-import "@/assets/scss/icons.scss";
-import "@/assets/scss/theme.scss";
+import 'bootstrap';
+import '@/assets/scss/bootstrap.scss';
+import '@/assets/scss/icons.scss';
+import '@/assets/scss/theme.scss';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   pwa: {
+    workboxPluginMode: 'InjectManifest',
     themeColor: '#fff',
     workboxOptions: {
       skipWaiting: true,
+      swSrc: 'service-worker.js',
     },
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'auto',
