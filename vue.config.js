1a6ad35df527b5759c378349089184baf86a1f14
Dilshat <dilshat.osmanov05@gmail.com>
2023-11-16 21:58:07 +0600

    service-worker.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,16 @@ module.exports = {
     themeColor: '#fff',
     workboxOptions: {
       swSrc: 'service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://smart-access-pi.vercel.app/'), // ваш API URL
+          handler: 'CacheFirst',
+        },
+        {
+          urlPattern: /\.(js|css|png|jpg|jpeg|svg)$/,
+          handler: 'CacheFirst',
+        },
+      ],
     },
     appleMobileWebAppCapable: 'yes',
   },
