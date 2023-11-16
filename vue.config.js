dd60b63d20ae4ded2b84490c03b7b6ab75c1b615
Dilshat <dilshat.osmanov05@gmail.com>
2023-11-16 19:46:24 +0600

    vue-config.js

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,6 @@ module.exports = {
       swSrc: 'service-worker.js',
     },
     appleMobileWebAppCapable: 'yes',
-    // appleMobileWebAppStatusBarStyle: 'transparent',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
   },
 };
