bd6c7f5d789f9e98bce7c52a1fc865991d7b2613
Dilshat <dilshat.osmanov05@gmail.com>
2023-11-16 21:08:20 +0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,7 @@
 const CACHE_NAME = 'my-app-cache-v1';
 
 // Список ресурсов для кэширования
-const cacheUrls = [
-  '/',
-  'index.html',
-  'css/',
-  'img/',
-  'js/', // Папка с JavaScript файлами
-];
+const cacheUrls = ['/', 'index.html', 'css/', 'fonts/', 'img/', 'js/'];
 
 // Установка сервис-воркера
 self.addEventListener('install', (event) => {
@@ -37,27 +31,34 @@ self.addEventListener('activate', (event) => {
 // Обработка запросов
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request).then((networkResponse) => {
-        if (
-          !networkResponse ||
-          networkResponse.status !== 200 ||
-          networkResponse.type !== 'basic'
-        ) {
-          return networkResponse;
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
         }
 
-        const clonedResponse = networkResponse.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, clonedResponse);
-        });
+        return fetch(event.request).then((networkResponse) => {
+          if (
+            !networkResponse ||
+            networkResponse.status !== 200 ||
+            networkResponse.type !== 'basic'
+          ) {
+            console.error('Fetch error for', event.request.url, 'Status:', networkResponse.status);
+            return networkResponse;
+          }
 
-        return networkResponse;
-      });
-    }),
+          const clonedResponse = networkResponse.clone();
+          // eslint-disable-next-line no-undef
+          caches.open(RUNTIME).then((cache) => {
+            cache.put(event.request, clonedResponse);
+          });
+
+          return networkResponse;
+        });
+      })
+      .catch((error) => {
+        console.error('Cache match error:', error);
+      }),
   );
 });
